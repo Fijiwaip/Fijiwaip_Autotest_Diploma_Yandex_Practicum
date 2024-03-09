f16046ffad3b4811c8b57e11df67bca1b49aa032
Igor <161233633+Fijiwaip@users.noreply.github.com>
2024-03-09 08:32:22 +0300

    Add files via upload

---
+++ configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://a6b4e174-c828-4b36-ba3e-7ef62df20c40.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"   # создание курьера
+CREATE_ORDER = "/api/v1/orders"  # создание заказа
+ORDER_INFORMATION = "/api/v1/orders/track?t="   # информация о заказе по его номеру
